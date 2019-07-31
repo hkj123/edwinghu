5f25fd3d21ce2d3918613797b9ec48e76d5afaac
hukaijia <190916516@qq.com>
2019-07-31 14:46:26 +0800

    springboot 2.0测试

---
+++ HELP.md
@@ -0,0 +1,16 @@
+# Getting Started
+
+### Reference Documentation
+For further reference, please consider the following sections:
+
+* [Official Apache Maven documentation](https://maven.apache.org/guides/index.html)
+* [Spring Boot DevTools](https://docs.spring.io/spring-boot/docs/{bootVersion}/reference/htmlsingle/#using-boot-devtools)
+* [Spring Boot Actuator](https://docs.spring.io/spring-boot/docs/{bootVersion}/reference/htmlsingle/#production-ready)
+
+### Guides
+The following guides illustrate how to use some features concretely:
+
+* [Accessing data with MySQL](https://spring.io/guides/gs/accessing-data-mysql/)
+* [Building a RESTful Web Service with Spring Boot Actuator](https://spring.io/guides/gs/actuator-service/)
+* [Using Spring Cloud Gateway](https://github.com/spring-cloud-samples/spring-cloud-gateway-sample)
+
---
+++ consul/src/main/java/com/springboot/consul/ConsulApplication.java
@@ -0,0 +1,77 @@
+package com.springboot.consul;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@RestController
+@EnableConfigurationProperties({ StudentConfig.class, TeachConfig.class })
+public class ConsulApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConsulApplication.class);
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private StudentConfig studentConfig;
+
+    @Autowired
+    private TeachConfig teachConfig;
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/user/description")
+    public String Description() {
+        return "description";
+    }
+
+    @GetMapping("/user/student/intro")
+    public String StudentIntro() {
+        return studentConfig.toString();
+    }
+
+    @GetMapping("/user/teach/intro")
+    public String TeachIntro() {
+        return teachConfig.toString();
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication.run(ConsulApplication.class, args);
+//        SpringApplication app = new SpringApplication(ConsulApplication.class);
+//        Environment env = app.run(args).getEnvironment();
+//        log.info("\n----------------------------------------------------------\n\t" +
+//                        "Application '{}' is running! Access URLs:\n\t" +
+//                        "Local: \t\thttp://localhost:{}\n\t" +
+//                        "External: \thttp://{}:{}\n\t" +
+//                        "SwaggerUI: \thttp://localhost:{}/swagger-ui.html\n" +
+//                        "----------------------------------------------------------",
+//                env.getProperty("spring.application.name"),
+//                env.getProperty("server.port"),
+//                InetAddress.getLocalHost().getHostAddress(),
+//                env.getProperty("server.port"),
+//                env.getProperty("server.port"));
+    }
+}
