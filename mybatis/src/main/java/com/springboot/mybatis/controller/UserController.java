49235b3884490bcbaa549b4e195d54e440bffe36
hukaijia <190916516@qq.com>
2019-09-12 11:44:18 +0800

    添加httpclient

--- mybatis/src/main/java/com/springboot/mybatis/controller/UserController.java
+++ mybatis/src/main/java/com/springboot/mybatis/controller/UserController.java
@@ -13,7 +13,9 @@ import com.nimbusds.jose.jwk.*;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.springboot.mybatis.entity.User;
+import com.springboot.mybatis.model.Test;
 import com.springboot.mybatis.service.UserService;
+import com.springboot.mybatis.utils.HttpClientUtil;
 import com.springboot.mybatis.utils.Result;
 import com.springboot.mybatis.utils.SerializeUtil;
 import org.json.simple.JSONObject;
@@ -24,9 +26,7 @@ import org.springframework.web.bind.annotation.*;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @RestController   //注意模板 需要这个
 @RequestMapping("/user")
@@ -54,55 +54,81 @@ public class UserController extends BaseController {
         return new Result(Result.ReturnValue.SUCCESS, "", userList);
     }
 
+//    public static void main(String[] args) {
+//        try {
+//
+//            BufferedReader reader1  = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("claims.json"),"UTF-8"));
+//            StringBuilder sb1 = new StringBuilder();
+//            String s1; // 依次循环，至到读的值为空
+//            while ((s1 = reader1.readLine()) != null) {
+//                sb1.append(s1);
+//            }
+//            reader1.close();
+//
+//            String requestObjectClaims = sb1.toString();
+//
+//            BufferedReader reader  = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("jwk.json"),"UTF-8"));
+//            StringBuilder sb = new StringBuilder();
+//            String s; // 依次循环，至到读的值为空
+//            while ((s = reader.readLine()) != null) {
+//                sb.append(s);
+//            }
+//            reader.close();
+//
+//            String jwks = sb.toString();
+//
+//            JWTClaimsSet claimSet = JWTClaimsSet.parse(requestObjectClaims);
+//            JWKSet jwkSet = JWKSet.parse(jwks);
+//            if (jwkSet.getKeys().size() == 1) {
+//                // figure out which algorithm to use
+//                JWK jwk = jwkSet.getKeys().iterator().next();
+//
+//                JWSSigner signer = null;
+//                if (jwk.getKeyType().equals(KeyType.RSA)) {
+//                    signer = new RSASSASigner((RSAKey) jwk);
+//                } else if (jwk.getKeyType().equals(KeyType.EC)) {
+//                    signer = new ECDSASigner((ECKey) jwk);
+//                } else if (jwk.getKeyType().equals(KeyType.OCT)) {
+//                    signer = new MACSigner((OctetSequenceKey) jwk);
+//                }
+//                //
+//                JWSHeader header = new JWSHeader(JWSAlgorithm.parse("RS256"), JOSEObjectType.JWT, null, null, null, null, null, null, null, null, "client1-RS256", null, null);
+//                SignedJWT requestObject = new SignedJWT(header, claimSet);
+//                requestObject.sign(signer);
+//                System.out.println("**********claims" + claimSet.toString());
+//                System.out.println("**********header" + header.toString());
+//                System.out.println("**********requestObject" + requestObject.serialize());
+//                System.out.println("**********key" + jwk.toJSONString());
+//            }
+//
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+
     public static void main(String[] args) {
-        try {
 
-            BufferedReader reader1  = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("claims.json"),"UTF-8"));
-            StringBuilder sb1 = new StringBuilder();
-            String s1; // 依次循环，至到读的值为空
-            while ((s1 = reader1.readLine()) != null) {
-                sb1.append(s1);
-            }
-            reader1.close();
+//        String url = "http://localhost:8080/auth/realms/openbanking/protocol/openid-connect/token";
+//        Map<String, String> headerBody = new HashMap<>();
+//        headerBody.put("Content-Type", "application/x-www-form-urlencoded");
+//
+//        Map<String, String> entityBody = new HashMap<>();
+//        entityBody.put("grant_type", "client_credentials");
+//        entityBody.put("client_id", "resource-proxy");
+//        entityBody.put("client_secret", "85a564ec-2824-4b2d-94f1-aa46f29f87a4");
+//
+//        String httpOrgCreateTestRtn = HttpClientUtil.doPost(url, headerBody, entityBody, "utf-8");
 
-            String requestObjectClaims = sb1.toString();
-
-            BufferedReader reader  = new BufferedReader(new InputStreamReader(Thread.currentThread().getContextClassLoader().getResourceAsStream("jwk.json"),"UTF-8"));
-            StringBuilder sb = new StringBuilder();
-            String s; // 依次循环，至到读的值为空
-            while ((s = reader.readLine()) != null) {
-                sb.append(s);
-            }
-            reader.close();
-
-            String jwks = sb.toString();
-
-            JWTClaimsSet claimSet = JWTClaimsSet.parse(requestObjectClaims);
-            JWKSet jwkSet = JWKSet.parse(jwks);
-            if (jwkSet.getKeys().size() == 1) {
-                // figure out which algorithm to use
-                JWK jwk = jwkSet.getKeys().iterator().next();
-
-                JWSSigner signer = null;
-                if (jwk.getKeyType().equals(KeyType.RSA)) {
-                    signer = new RSASSASigner((RSAKey) jwk);
-                } else if (jwk.getKeyType().equals(KeyType.EC)) {
-                    signer = new ECDSASigner((ECKey) jwk);
-                } else if (jwk.getKeyType().equals(KeyType.OCT)) {
-                    signer = new MACSigner((OctetSequenceKey) jwk);
-                }
-                //
-                JWSHeader header = new JWSHeader(JWSAlgorithm.parse("RS256"), JOSEObjectType.JWT, null, null, null, null, null, null, null, null, "client1-RS256", null, null);
-                SignedJWT requestObject = new SignedJWT(header, claimSet);
-                requestObject.sign(signer);
-                System.out.println("**********claims" + claimSet.toString());
-                System.out.println("**********header" + header.toString());
-                System.out.println("**********requestObject" + requestObject.serialize());
-                System.out.println("**********key" + jwk.toJSONString());
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        String url = "https://qloudpdp.pditdap.service.sd/auth/realms/openbankingrs256/protocol/openid-connect/token";
+//        Map<String, String> headerBody = new HashMap<>();
+//        headerBody.put("Content-Type", "application/x-www-form-urlencoded");
+//
+//        Map<String, String> entityBody = new HashMap<>();
+//        entityBody.put("grant_type", "client_credentials");
+//        entityBody.put("client_id", "client1-mtls-RS256-PS256");
+//        entityBody.put("scope", "accounts");
+//
+//        String httpOrgCreateTestRtn = HttpClientUtil.doPost(url, headerBody, entityBody, "utf-8");
     }
 }
