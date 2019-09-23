652abec98bcf027c7fcd31f7ca3cb98d5c35d6f0
hukaijia <190916516@qq.com>
2019-09-23 15:08:08 +0800

    添加eureka注册中心

---
+++ eureka/src/main/java/com/springboot/eureke/EurekaApplication.java
@@ -0,0 +1,42 @@
+package com.springboot.eureke;
+
+import com.netflix.discovery.DiscoveryClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@EnableEurekaServer
+//@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
+public class EurekaApplication {
+    private static final Logger log = LoggerFactory.getLogger(EurekaApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+//        new SpringApplicationBuilder(EurekaApplication.class).run(args);
+    }
+}
+//		http://localhost:1111/
+
+//@RestController
+//class ServiceInstanceRestController {
+//
+//    @Autowired
+//    private DiscoveryClient discoveryClient;
+//
+//    @RequestMapping("/service-instances/{applicationName}")
+//    public List<ServiceInstance> serviceInstancesByApplicationName(
+//            @PathVariable String applicationName) {
+//        return this.discoveryClient.getInstances(applicationName);
+//    }
+//}
