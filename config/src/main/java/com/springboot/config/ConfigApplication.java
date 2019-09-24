e79a178cad84242d7cd0e6e7572a6723ab5ff9f7
hukaijia <190916516@qq.com>
2019-09-24 15:22:36 +0800

    添加config配置

--- config/src/main/java/com/springboot/config/ConfigApplication.java
+++ config/src/main/java/com/springboot/config/ConfigApplication.java
@@ -12,4 +12,5 @@ public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
     }
+//    http://localhost:8781/product-dev.properties
 }
