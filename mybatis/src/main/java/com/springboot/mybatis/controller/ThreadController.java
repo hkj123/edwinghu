bdbf6e78faffeed65d16f2194f1e7728be980549
hukaijia <190916516@qq.com>
2020-01-03 16:43:09 +0800

    删除配置文件

--- mybatis/src/main/java/com/springboot/mybatis/controller/ThreadController.java
+++ mybatis/src/main/java/com/springboot/mybatis/controller/ThreadController.java
@@ -2,9 +2,19 @@ package com.springboot.mybatis.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+//New：新创建的线程，尚未执行；
+//Runnable：运行中的线程，正在执行run()方法的Java代码；
+//Blocked：运行中的线程，因为某些操作被阻塞而挂起；
+//Waiting：运行中的线程，因为某些操作在等待中；
+//Timed Waiting：运行中的线程，因为执行sleep()方法正在计时等待；
+//Terminated：线程已终止，因为run()方法执行完毕
 @RestController   //注意模板 需要这个
 public class ThreadController extends BaseController {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Thread t = new MyThread();
         t.start(); // 启动新线程
 
@@ -31,6 +41,10 @@ public class ThreadController extends BaseController {
             Thread.sleep(20);
         } catch (InterruptedException e) {}
         System.out.println("main end...");
+
+//        java.util.concurrent 包类详解
+        Counter t4 = new Counter();
+        t4.add(1);
     }
 }
 
@@ -45,5 +59,20 @@ class MyRunnable implements Runnable {
     @Override
     public void run() {
         System.out.println("线程thread implements Runnable");
+    }
+}
+
+class Counter{
+    private final Lock lock = new ReentrantLock();
+    private int count;
+
+    public void add(int n) throws Exception{
+        if (lock.tryLock(1, TimeUnit.SECONDS)) {
+            try {
+                count += n;
+            } finally {
+                lock.unlock();
+            }
+        }
     }
 }
